458d7e3e82c28e53bd37c261ae5f29f1a8873b26
Vinicius Alexander Moreira Nascimento <vinicius.malex2002@gmail.com>
2024-06-10 23:59:34 -0300

    Implement the navbar and chage the button to add a new showcase

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,8 +4,13 @@ import StarRating from "../../assets/starRating";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+<<<<<<< HEAD
   const [vitrines, setVitrines] = useState([]);
   const contentRefs = useRef([]);
+=======
+  const contentRef = useRef(null);
+
+>>>>>>> b44feff (Implement the navbar and chage the button to add a new showcase)
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -33,6 +38,7 @@ const Home = () => {
   return (
     <>
       <div className="container">
+<<<<<<< HEAD
         {vitrines.map((vitrine, index) => (
           <div key={vitrine.id} className="vitrine-container">
             <h3>{vitrine.title}</h3>
@@ -53,10 +59,24 @@ const Home = () => {
                   <p>R$ {product.price}</p>
                   <p style={{ fontSize: "12px", color: "green" }}>
                     (10% de desconto no pix)
+=======
+        <h2> Ofertas feitas para você </h2>
+        <div className="content" ref={contentRef}>
+          {products.map((p) => (
+            <div key={p.id}>
+              <div className="product">
+                <img src={p.image} alt="image" />
+                <p>{p.title}</p>
+                <div className="rating">
+                  <StarRating rate={p.rating.rate} />
+                  <p style={{ fontSize: "10px" }}>
+                    {p.rating.rate} ({p.rating.count})
+>>>>>>> b44feff (Implement the navbar and chage the button to add a new showcase)
                   </p>
                 </div>
               ))}
             </div>
+<<<<<<< HEAD
             <button
               className="scroll-button left"
               onClick={() => handleScroll(index, -100)}
@@ -71,6 +91,22 @@ const Home = () => {
             </button>
           </div>
         ))}
+=======
+          ))}
+        </div>
+        <button
+          className="scroll-button left"
+          onClick={() => handleScroll(-100)}
+        >
+          &#10094;
+        </button>
+        <button
+          className="scroll-button right"
+          onClick={() => handleScroll(100)}
+        >
+          &#10095;
+        </button>
+>>>>>>> b44feff (Implement the navbar and chage the button to add a new showcase)
       </div>
     </>
   );
